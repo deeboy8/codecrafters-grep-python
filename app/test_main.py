c8664d92db70d9d1a075c9ff7c7c42183021408c
Demitrus Booker <demitrusw.b@gmail.com>
2024-09-27 20:35:43 -0700

    adding ut test file

---
+++ app/test_main.py
@@ -0,0 +1,30 @@
+import pytest
+
+from main import match_pattern 
+
+def test_digit():
+    pattern = '\\d'
+    input_line_pass = '123'
+    input_line_fail = 'abc'
+
+    test_pass = match_pattern(input_line_pass, pattern)
+    test_fail = match_pattern(input_line_fail, pattern)
+    
+    assert(test_pass)
+    assert (not test_fail)
+
+# def test_negative_character_classes():
+#     pattern = "[^abc]"
+#     input_line = 'def'
+
+#     test_pass = match_pattern(input_line, pattern)
+
+#     assert(test_pass)
+
+# def test_negative_character_class():
+#     pattern = "[^abc]"
+#     input_line = 'abc'
+
+#     test_pass = match_pattern(input_line, pattern)
+
+#     assert(not test_pass)
