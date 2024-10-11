3a09e76436213f46121f1aa7dae0aeb551d48f3d
Demitrus Booker <demitrusw.b@gmail.com>
2024-10-10 21:09:41 -0700

    added code for ^ at beginning of pattern at very beginning of match_pattern fx to compare pattern and input_line directly as a new function

--- app/main.py
+++ app/main.py
@@ -3,22 +3,25 @@ import sys
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
 
+def check_for_pattern_match(pattern, new_input_line):
+    return pattern == new_input_line
+        
+
 def match_pattern(input_line, pattern):
+    if pattern.startswith('^'):
+        pattern = pattern[1:]
+        len_of_pattern = len(pattern)
+        new_input_line = input_line[:len_of_pattern]
+        check_for_pattern_match(pattern, new_input_line)
+        if not check_for_pattern_match:
+            return False
+        
     i, j = 0, 0
     while i < len(pattern) and j < len(input_line):
         # loop over alphanumeric characters in input line if no match with pattern
-        if pattern[i] == input_line[j]:
+        if pattern[i] == input_line[j]: ## what if you had /d in input_line
             i += 1; j += 1
             continue
-        # if find digit character class tags
-        # elif pattern[i] == '\\':
-        #     i += 1 # move indice to d or w char
-        #     if i < len(pattern):
-        #         if pattern[i] == 'd' and input_line[j].isdigit() \
-        #         or pattern[i] == 'w' and input_line[j].isalnum():
-        #             i += 1; j += 1
-        #         else:
-        #             return False
         if pattern[i] == '\\':
             i += 1
             if i < len(pattern):
@@ -26,7 +29,7 @@ def match_pattern(input_line, pattern):
                     if not input_line[j].isdigit():
                         j += 1; i -= 1
                         continue
-                elif pattern[i] == 'w':
+                elif pattern[i] == 'w': ##
                     if not input_line[j].isalnum():
                         j += 1
                         continue
@@ -34,6 +37,7 @@ def match_pattern(input_line, pattern):
                 j += 1
             else:
                 return False
+        #TODO: good place for error handling -> if \\ but doesn't match d/w then its a problem
         # if find negative or positive character groups in pattern
         elif pattern[i] == '[':
             closing_bracket = pattern.find(']', i)
@@ -46,7 +50,6 @@ def match_pattern(input_line, pattern):
                     return False
             else:
                 if input_line[j] not in char_set:
-                    # return True
                     j += 1
                     continue
             i = closing_bracket + 1
@@ -57,14 +60,14 @@ def match_pattern(input_line, pattern):
     return i == len(pattern)
       
 def main():
-    pattern = sys.argv[2]
-    # pattern = '\\d apples'
-    input_line = sys.stdin.read()
-    # input_line = "sally has 1 oranges" #input("Enter input_line: ")
+    # pattern = sys.argv[2]
+    pattern = '^sally has 1 orange'
+    # input_line = sys.stdin.read()
+    input_line = "sally has 1 orange" #input("Enter input_line: ")
 
-    if sys.argv[1] != "-E":
-        print("Expected first argument to be '-E'")
-        exit(1)
+    # if sys.argv[1] != "-E":
+    #     print("Expected first argument to be '-E'")
+    #     exit(1)
 
     # # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
